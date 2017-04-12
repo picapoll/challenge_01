63614d328ee074d4f01d4755659737905e1cd2b4
Bernat Moragas <bmoragas@gmail.com>
2017-04-12 10:28:30 +0200

    Second round of the first exercices

--- multiplications_table.js
+++ multiplications_table.js
@@ -4,9 +4,11 @@ Multiplications table
 Write a function that writes in the console the multiplication table (from 1 to 10)
 
 Hint: https://msdn.microsoft.com/en-us/library/2yfce773(v=vs.94).aspx */
-
-var res= '\n'
-for (var i = 0; i<10;i++){
-	j=1}
-	res+='*'
+var multiplications = "";
+for (i = 0; i<=10; i++){
+	for (j=1; j<=10; j++){ 
+		//return i*j;
+		multiplications+= i*j + " ";
+	}
 }
+console.log(multiplications)
